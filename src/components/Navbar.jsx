33ee7ab9bbde4671e429a60b5cb9b83211e1daec
bhavin <bhavin.matreja@gmail.com>
2025-12-26 20:08:16 +0530

    Added Search functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { assets } from '../assets/assets';
 import { NavLink } from 'react-router-dom';
 import { useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
 
 const Navbar = () => {
 
     const [visible, setVisible] = useState(false)
+    const {setShowSearch} = useContext(ShopContext)
 
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
@@ -34,7 +36,7 @@ const Navbar = () => {
       </ul>
 
       <div className='flex items-center gap-6'>
-            <img src={assets.search_icon} className='cursor-pointer w-5' alt="" />
+            <img onClick={()=> setShowSearch(true)} src={assets.search_icon} className='cursor-pointer w-5' alt="" />
             <div className='group relative'>
                 <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" />
                 <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
