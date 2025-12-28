6c03d695fab96fa3988c19fbdffbb74e2d1dd1b8
bhavin <bhavin.matreja@gmail.com>
2025-12-28 12:36:42 +0530

    added navigate to place-order page from cart

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect } from "react";
 import { products } from "../assets/assets";
 import { useState } from "react";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -12,6 +13,7 @@ const ShopContextProvider = (props) => {
     const [search, setSearch] = useState('')
     const [showSearch, setShowSearch] = useState(true)
     const [cartItems, setCartItems] = useState({})
+    const navigate = useNavigate()
 
     const addToCart = async(itemId, size) => {
 
@@ -88,7 +90,7 @@ const ShopContextProvider = (props) => {
         search, setSearch, showSearch, setShowSearch,
         cartItems, addToCart,
         getCardCount, updateQuantity,
-        getCartAmount
+        getCartAmount, navigate
     }
 
     return (
